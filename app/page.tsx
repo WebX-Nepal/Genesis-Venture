a141acb55135408cde368842ff89f71ac7462cc6
Abhishek Subedi <subediabhishek2060@gmail.com>
2026-04-05 14:18:26 +0545

    investment flyer

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
+"use client";
+
+import { useState } from "react";
 import About from "@/components/Home/About";
 import FAQ from "@/components/Home/FAQ";
 import Hero from "@/components/Home/Hero";
 import Sectors from "@/components/Home/Sectors";
+import InvestmentRiskModal from "@/components/InvestmentRiskFlyer";
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(true);
+
   return (
     <main className="w-full font-[PPFONT]">
+      {showModal && <InvestmentRiskModal onClose={() => setShowModal(false)} />}
       <Hero />
       <About />
       <Sectors />
-      {/* <Portfolio /> */}
       <FAQ />
-      {/* <Contact /> */}
     </main>
   );
 }
