2cc7666f32ee00278ce2d0a3a37cd12a43857a8d
sijanxt <sijanbartaula39@gmail.com>
2026-05-05 15:56:35 +0545

    fix:issues

--- components/Home/devider.tsx
+++ components/Home/devider.tsx
@@ -5,7 +5,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Title from "../ui/Title";
+
 
 gsap.registerPlugin(SplitText, ScrollTrigger);
 export default function Devider() {
