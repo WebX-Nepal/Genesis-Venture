f2b837779e721611d7b72e0f642c6a427ce381fb
sijanxt <sijanbartaula39@gmail.com>
2026-05-18 17:16:58 +0545

    fx:images issues

--- components/About/InvestmentPhilosophy.tsx
+++ components/About/InvestmentPhilosophy.tsx
@@ -12,7 +12,7 @@ const philosophyRows = [
   {
     title: "Conviction Over Noise",
     body: "We prioritize long-term fundamentals over short-term headlines. Our decisions are built on business quality, governance strength, and clarity of execution.",
-    image: "/coin/image.png",
+    image: "/coin/1.png",
   },
   {
     title: "Early Entry Advantage",
